e2ecfc3b95c9b4d15784588af81d2be9990b6456
umepon0626 <ryuichiastrona@gmail.com>
2022-05-13 19:44:13 +0900

    selectの状態をページで持つようにした。

--- next-practice/components/listPrefecture.tsx
+++ next-practice/components/listPrefecture.tsx
@@ -2,12 +2,18 @@ import { useQuery } from 'react-query'
 import { AxiosPromise } from 'axios'
 import { PrefectureRepository } from '../repositories/resas/prefectures'
 import { PrefecturesResponse } from '../entities/prefecture'
-import Select, {MultiValue} from 'react-select'
-import { useState } from 'react'
+import Select, { MultiValue } from 'react-select'
 
-export const ListPrefectureView: React.FC = () => {
+type SelectOption = {value: Number; label: string};
+
+type Props = {
+  selectedOptions: MultiValue<SelectOption>;
+  setSelectedOptions: (selectedValue: MultiValue<SelectOption>) => void;
+}
+
+export const ListPrefectureView: React.FC<Props> = (props) => {
   const prefectureRepository = new PrefectureRepository();
-  const [selectedOptions, setSelectedOptions] = useState<MultiValue<{value: Number; label: string}>>([]);
+  const {selectedOptions, setSelectedOptions} = props;
   const prefectures = useQuery(
     ['prefecture'], 
     (): AxiosPromise<PrefecturesResponse> =>
--- next-practice/pages/index.tsx
+++ next-practice/pages/index.tsx
@@ -5,13 +5,23 @@ import styles from '../styles/Home.module.css'
 import { ListPrefectureView } from '../components/listPrefecture'
 import { QueryClient, QueryClientProvider } from 'react-query'
 
+import Select, { MultiValue } from 'react-select'
+import {useState} from 'react'
+
+type SelectOption = {value: Number; label: string};
+
+type Props = {
+  selectedOptions: MultiValue<SelectOption>;
+  setSelectedOptions: (selectedValue: MultiValue<SelectOption>) => void;
+}
 
 const queryClient = new QueryClient()
 
 const Home: NextPage = () => {
+  const [selectedOptions,setSelectedOptions] = useState<MultiValue<SelectOption>>([])
   return (
     <QueryClientProvider client={queryClient}>
-      <ListPrefectureView/>
+      <ListPrefectureView selectedOptions={selectedOptions} setSelectedOptions={setSelectedOptions}/>
     </QueryClientProvider>
   )
 }
