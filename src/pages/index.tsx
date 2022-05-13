7fa1296cd40cc5467e6afec65081d973b6936bea
umepon0626 <ryuichiastrona@gmail.com>
2022-05-14 08:44:33 +0900

    ページにグラフを設置

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import { ListPrefectureView } from '../components/listPrefecture'
+import { PopulationGraphView } from '../components/populationGraph'
 import { QueryClient, QueryClientProvider } from 'react-query'
 
-import Select, { MultiValue } from 'react-select'
+import { MultiValue } from 'react-select'
 import {useState} from 'react'
 
 type SelectOption = {value: Number; label: string};
@@ -22,6 +20,8 @@ const Home: NextPage = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <ListPrefectureView selectedOptions={selectedOptions} setSelectedOptions={setSelectedOptions}/>
+      {selectedOptions.length  ? <PopulationGraphView prefCodes={selectedOptions.map(option => option.value)} prefLabels={selectedOptions.map(option => option.label)}/> : <>選択してください</>}
+      
     </QueryClientProvider>
   )
 }
